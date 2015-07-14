43c5e78dcbeb530cd500533bda4a05158a90a308
Greg Karékinian <greg@karekinian.com>
2015-07-14 15:10:42 +0200

    Don't depend on a specific version of the application cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      "Deploys and configures PHP-based applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.0"
 
-depends "application", "~> 3.0"
+depends "application"
 depends "apache2"
 depends "php"
